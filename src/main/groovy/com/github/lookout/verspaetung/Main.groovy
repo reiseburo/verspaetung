70f0d61e7b331a002ca46cb84e8f120e5418705a
R. Tyler Croy <tyler@monkeypox.org>
2015-01-17 14:42:24 -0800

    Exploring using curator's TreeCache for grabbing an entire ZK sub-tree

--- src/main/groovy/com/github/lookout/verspaetung/Main.groovy
+++ src/main/groovy/com/github/lookout/verspaetung/Main.groovy
@@ -2,15 +2,42 @@ package com.github.lookout.verspaetung
 
 import org.apache.curator.retry.ExponentialBackoffRetry
 import org.apache.curator.framework.CuratorFrameworkFactory
+import org.apache.curator.framework.CuratorFramework
 import org.apache.zookeeper.KeeperException
+import org.apache.curator.framework.recipes.cache.TreeCache
+import org.apache.curator.framework.recipes.cache.TreeCacheListener
 
 class Main {
     static void main(String[] args) {
-        println "Running!"
+        println "Running ${args}"
 
         ExponentialBackoffRetry retry = new ExponentialBackoffRetry(1000, 3)
-        def client = CuratorFrameworkFactory.newClient('lookout-zk-stage1-0.flexilis.org:2181', retry)
+        CuratorFramework client = CuratorFrameworkFactory.newClient(args[0], retry)
         client.start()
+        TreeCache cache = new TreeCache(client, '/kafka_spout')
+        println cache
+
+        cache.listenable.addListener([
+            childEvent: { cl, ev ->
+                println "EV: ${ev}"
+            }
+        ] as TreeCacheListener)
+
+
+        cache.start()
+        println 'started..'
+
+        Boolean foundChildren = false
+        Map children = null
+
+        while ((children == null) || children.isEmpty()) {
+            children = cache.getCurrentChildren('/kafka_spout')
+            println "CHILDREN: ${children}"
+
+            Thread.sleep(100)
+        }
+        [1, 2, 3].each { Thread.sleep(300) }
+        return
 
         client.children.forPath('/consumers').each { path ->
             try {
